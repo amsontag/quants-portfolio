b494791f3f2b696d80436b957abb46f59997548c
amsontag <71695469+amsontag@users.noreply.github.com>
2025-11-03 22:16:30 +0000

    Initial commit: scaffolded with Cookiecutter Data Science

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Repository to manage Quantitative Research and Data Science Projects.',
+    author='Andrei Sontag',
+    license='MIT',
+)
